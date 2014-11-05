9df3f6b596ecfbae15579f281cec8d8ae750e899
Mauro Pinto <mauro.s.pinto@gmail.com>
2014-11-05 12:01:08 +0000

    rvm path

---
+++ source/50_rvm.sh
@@ -0,0 +1,7 @@
+# Ruby Version Manager
+
+# Load RVM into a shell session *as a function*
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
+
+# Add RVM to PATH for scripting
+export PATH="$PATH:$HOME/.rvm/bin"
