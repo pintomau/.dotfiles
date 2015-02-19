49f561fa79d4509b8ae6f707d74600ccc13253d2
Mauro Pinto <mauro.s.pinto@gmail.com>
2015-02-19 17:20:46 +0000

    adds phpspec aliases

---
+++ source/50_php.sh
@@ -0,0 +1,9 @@
+# PHPSpec Command
+
+alias phpspec='vendor/bin/phpspec'
+
+# PHPSpec Shortcuts
+
+alias spec='vendor/bin/phpspec'
+alias specr='vendor/bin/phpspec run'
+alias specd='vendor/bin/phpspec describe'
