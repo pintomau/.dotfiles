cb8aba29ac51948a02cc1bbc7bcb41350562cbce
Mauro Pinto <mauro.s.pinto@gmail.com>
2015-04-24 10:44:53 +0100

    laravel artisan alias

--- source/50_php.sh
+++ source/50_php.sh
@@ -2,8 +2,22 @@
 
 alias phpspec='vendor/bin/phpspec'
 
+
+
 # PHPSpec Shortcuts
 
 alias spec='vendor/bin/phpspec'
 alias specr='vendor/bin/phpspec run'
 alias specd='vendor/bin/phpspec describe'
+
+
+
+# Laravel
+
+alias artisan='php artisan'
+
+
+
+# Homestead path
+
+export PATH="$PATH:$HOME/.composer/vendor/bin"
