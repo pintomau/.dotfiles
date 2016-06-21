f9adf0f2e7dabcb7bfd3ab11c62a245f112ec6b2
Mauro Pinto <mauro.s.pinto@gmail.com>
2016-06-21 21:31:33 +0100

    chekc if virtualenv exists before

--- source/50_ubuntu_python.sh
+++ source/50_ubuntu_python.sh
@@ -4,7 +4,10 @@ is_ubuntu || return 1
 export WORKON_HOME=~/.virtualenvs
 
 # Run Virtualenvwrapper script
-source "$HOME/.local/bin/virtualenvwrapper.sh"
+if [ -f "$HOME/.local/bin/virtualenvwrapper.sh" ]
+then
+    source "$HOME/.local/bin/virtualenvwrapper.sh"
+fi
 
 # Add packages path
 export PATH="$PATH:$HOME/.local/lib/python2.7/site-packages"
