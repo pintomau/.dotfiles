7c29aaf16c616344c09c52a5d7f4bc9ed6140a32
Mauro Pinto <mauro.s.pinto@gmail.com>
2020-02-28 13:33:18 +0000

    comment on grep

--- source/50_aliases.sh
+++ source/50_aliases.sh
@@ -1,4 +1,4 @@
-# Always enable colored `grep` output
+# Always enable colored `grep` output and case insensitive search
 alias grep="grep --color=auto -i"
 alias fgrep="fgrep --color=auto -i"
 alias egrep="egrep --color=auto -i"
