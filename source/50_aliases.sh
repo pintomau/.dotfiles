c6745d851fb81f3cec2f72d53323b0c54f98bfcc
Mauro Pinto <mauro.s.pinto@gmail.com>
2020-07-21 16:25:05 +0100

    add portopen alias

--- source/50_aliases.sh
+++ source/50_aliases.sh
@@ -2,3 +2,13 @@
 alias grep="grep --color=auto -i"
 alias fgrep="fgrep --color=auto -i"
 alias egrep="egrep --color=auto -i"
+
+# Networking
+alias portopen="nc -vz" # portopen google.com 443
+##  IP addresses
+alias wanip="dig +short myip.opendns.com @resolver1.opendns.com"
+alias whois="whois -h whois-servers.net"
+##  Flush Directory Service cache
+alias flush="dscacheutil -flushcache"
+## View HTTP traffic
+alias httpdump="sudo tcpdump -i en1 -n -s 0 -w - | grep -a -o -E \"Host\: .*|GET \/.*\""
